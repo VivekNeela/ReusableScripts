2b9c15824fdbb3f5ff810a027e579d72459fe6c3
VivekNeela <viveks@neelafilm.com>
2025-03-17 19:22:21 +0530

    added OnTapObject
    ...

---
+++ ReusableScripts/OnTapObject.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using Unity.IO.LowLevel.Unsafe;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace TMKOC.Rhymes
+{
+    public abstract class OnTapObject : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+    {
+        public abstract void OnMouseDown();
+        public abstract void OnPointerDown(PointerEventData eventData);
+        public abstract void OnPointerUp(PointerEventData eventData);
+        public virtual void OnDrag(PointerEventData eventData) { }
+
+        protected virtual void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                ClickedOnce();
+            }
+        }
+
+        protected virtual void ClickedOnce() { }
+
+
+
+
+    }
+}
