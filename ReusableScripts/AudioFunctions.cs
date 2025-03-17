500d8c1ad3088258c44eaed944672a78957fd852
VivekNeela <viveks@neelafilm.com>
2025-03-17 18:25:52 +0530

    Initial Commit
    Reusable scripts package...

---
+++ ReusableScripts/AudioFunctions.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TMKOC.Reusable
+{
+    
+    public class SingletonAudioManager : SingletonMonobehaviour<AudioFunctions>
+    {
+        //this exists only to make a the audio functions class a singleton...
+    }
+
+    
+    public class AudioFunctions : MonoBehaviour
+    {
+
+        public AudioSource audioSource_BG;
+        public AudioSource audioSource;
+        [SerializeField] private bool isPlaying;
+        public bool IsPlaying { get => isPlaying; set => isPlaying = value; }
+
+
+
+        private void Update() => IsPlaying = audioSource.isPlaying;
+
+        public void PlayOnce(AudioClip clip, Action nextClip = null)
+        {
+            // stopPlaying = false;
+            if (clip == null)
+            {
+                Debug.LogWarning("<color=yellow> AudioClip is null ! </color>");
+                StartCoroutine(WaitForClipEnd(nextClip));   //need to do this in case the clip is null and we still need to execute the next callback...(stupid af) 
+                return;
+            }
+
+            audioSource.clip = clip;
+            audioSource.loop = false;
+            audioSource.Play();
+
+            StartCoroutine(WaitForClipEnd(nextClip));
+
+        }
+
+
+        public void PlayOnLoop(AudioClip clip)
+        {
+            if (clip == null)
+            {
+                Debug.LogWarning("AudioClip is null.");
+                return;
+            }
+
+            audioSource.clip = clip;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+
+
+        public void StopPlaying()
+        {
+            audioSource.Stop();
+        }
+
+        private IEnumerator WaitForClipEnd(Action onClipEnd)
+        {
+            while (audioSource.isPlaying)
+            {
+                yield return null; // Wait for the next frame
+            }
+            onClipEnd?.Invoke();
+        }
+
+        public void PlayBgMusic(AudioClip clip)   //just different audiosource...
+        {
+            if (clip == null)
+            {
+                Debug.LogWarning("AudioClip is null.");
+                return;
+            }
+
+            audioSource_BG.clip = clip;
+            audioSource_BG.loop = true;
+            audioSource_BG.Play();
+        }
+
+    }
+
+
+}
---
+++ ReusableScripts/SingletonMonobehaviour.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace TMKOC.Reusable
+{
+    //this is a reusable script...
+    public class SingletonMonobehaviour<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T _instance;
+        // private static readonly object _lock = new object();   // Not needed in Unity, since it is single-threaded, but interesting to know.
+        private static bool _applicationIsQuitting = false;
+
+        [Header("This is a Singleton Monobehaviour")]
+        public bool isPersistent = true;
+
+
+        /// <summary>
+        /// Gets the Singleton instance of the class.
+        /// </summary>
+        public static T Instance
+        {
+            get
+            {
+                if (_applicationIsQuitting)
+                {
+                    Debug.LogWarning($"[Singleton] Instance of {typeof(T)} is already destroyed. Returning null.");
+                    return null;
+                }
+
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<T>();
+
+                    if (_instance == null)
+                    {
+                        GameObject singletonObject = new GameObject(typeof(T).Name);
+                        _instance = singletonObject.AddComponent<T>();
+
+                        //do we need to do this here???
+                        // DontDestroyOnLoad(singletonObject);
+
+
+                    }
+                }
+
+                return _instance;
+
+            }
+        }
+
+        /// <summary>
+        /// Ensures that the Singleton is properly initialized.
+        /// </summary>
+        protected virtual void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this as T;
+
+                if (isPersistent)
+                    DontDestroyOnLoad(gameObject);
+
+            }
+            else if (_instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        /// <summary>
+        /// Marks the application as quitting to avoid accessing destroyed instances.
+        /// </summary>
+        protected virtual void OnApplicationQuit()
+        {
+            _applicationIsQuitting = true;
+        }
+
+        /// <summary>
+        /// Cleans up when the instance is destroyed.
+        /// </summary>
+        protected virtual void OnDestroy()
+        {
+            if (_instance == this)
+            {
+                _instance = null;
+            }
+        }
+
+
+
+
+    }
+}
