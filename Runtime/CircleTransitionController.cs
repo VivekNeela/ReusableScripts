6b6f260d76c97fba7e0121cc37077861cdf8c464
VivekNeela <viveks@neelafilm.com>
2025-03-18 15:42:23 +0530

    updated Circle Transition controller

--- Runtime/CircleTransitionController.cs
+++ Runtime/CircleTransitionController.cs
@@ -4,14 +4,39 @@ using UnityEngine;
 
 public class CircleTransitionController : MonoBehaviour
 {
+    [SerializeField] private Canvas canvas;
+    [SerializeField] private RectTransform circleRect;
     [SerializeField] private Material transitionMat;
     [SerializeField] private float transitionDuration;
     [SerializeField] private bool inTransition;
+    public bool InTransition { get => inTransition; set => inTransition = value; }
 
+
+    private void Start()
+    {
+        //some random ass scaling logic below dosent makes much sense, just wanted to try this...
+        var canvasSize = canvas.GetComponent<RectTransform>().sizeDelta;
+        //scale up circle to the screen...
+        var side = canvasSize.x > canvasSize.y ? canvasSize.x : canvasSize.y;
+        circleRect.sizeDelta = new Vector2(side, side);
+    }
+
+
+
+
+    public void CloseCircle()
+    {
+        StartCoroutine(TransitionLerp(1, -0.1f, transitionDuration));
+    }
+
+    public void OpenCircle()
+    {
+        StartCoroutine(TransitionLerp(-0.1f, 1f, transitionDuration));
+    }
 
     public IEnumerator TransitionLerp(float a, float b, float duration)
     {
-        inTransition = true;
+        InTransition = true;
         float time = 0;
         while (time < duration)
         {
@@ -22,7 +47,7 @@ public class CircleTransitionController : MonoBehaviour
             yield return null;
         }
         transitionMat.SetFloat("_Radius", b);
-        inTransition = false;
+        InTransition = false;
     }
 
 }
