60f360e2ce2894e74d92869f810531fd5dfc9308
VivekNeela <viveks@neelafilm.com>
2025-08-01 19:08:05 +0530

    put draggable in reusable namespace
    ...

--- Runtime/Draggable.cs
+++ Runtime/Draggable.cs
@@ -1,65 +1,68 @@
 using UnityEngine;
 using Lean.Touch;
 using UnityEngine.Events;
-using Lean.Common; 
+using Lean.Common;
 
-
-[RequireComponent(typeof(SpriteRenderer), typeof(BoxCollider2D), typeof(LeanDragTranslate))]
-[RequireComponent(typeof(LeanSelectableByFinger))]
-public abstract class Draggable : MonoBehaviour
+namespace TMKOC.Reusable
 {
-    [SerializeField] protected SpriteRenderer spriteRenderer;
-    [SerializeField] protected LeanDragTranslate leanDragTranslate;
-    [SerializeField] protected LeanSelectableByFinger leanSelectableByFinger;
-
-    //only for setting up action events in code
-    private UnityAction<LeanSelect> OnSelectedAction;
-    private UnityAction<LeanSelect> OnDeselectedAction;
-
-
-    private void OnValidate()
+    [RequireComponent(typeof(SpriteRenderer), typeof(BoxCollider2D), typeof(LeanDragTranslate))]
+    [RequireComponent(typeof(LeanSelectableByFinger))]
+    public abstract class Draggable : MonoBehaviour
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        leanDragTranslate = GetComponent<LeanDragTranslate>();
-        leanSelectableByFinger = GetComponent<LeanSelectableByFinger>();
-    }
+        [SerializeField] protected SpriteRenderer spriteRenderer;
+        [SerializeField] protected LeanDragTranslate leanDragTranslate;
+        [SerializeField] protected LeanSelectableByFinger leanSelectableByFinger;
 
-    protected virtual void Awake()
-    {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        leanDragTranslate = GetComponent<LeanDragTranslate>();
-        leanSelectableByFinger = GetComponent<LeanSelectableByFinger>();
+        //only for setting up action events in code
+        private UnityAction<LeanSelect> OnSelectedAction;
+        private UnityAction<LeanSelect> OnDeselectedAction;
 
-        //settig selected and deselected in awake...
-        OnSelectedAction = (LeanSelect) =>
+
+        private void OnValidate()
         {
-            OnSelected();
-        };
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            leanDragTranslate = GetComponent<LeanDragTranslate>();
+            leanSelectableByFinger = GetComponent<LeanSelectableByFinger>();
+        }
 
-        OnDeselectedAction = (LeanSelect) =>
+        protected virtual void Awake()
         {
-            OnDeselected();
-        };
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            leanDragTranslate = GetComponent<LeanDragTranslate>();
+            leanSelectableByFinger = GetComponent<LeanSelectableByFinger>();
+
+            //settig selected and deselected in awake...
+            OnSelectedAction = (LeanSelect) =>
+            {
+                OnSelected();
+            };
+
+            OnDeselectedAction = (LeanSelect) =>
+            {
+                OnDeselected();
+            };
+
+        }
+
+        protected virtual void OnEnable()
+        {
+            leanSelectableByFinger.OnSelected.AddListener(OnSelectedAction);
+            leanSelectableByFinger.OnDeselected.AddListener(OnDeselectedAction);
+        }
+
+        protected virtual void OnDisable()
+        {
+            leanSelectableByFinger.OnSelected.RemoveListener(OnSelectedAction);
+            leanSelectableByFinger.OnDeselected.RemoveListener(OnDeselectedAction);
+        }
+
+        protected abstract void OnSelected();
+
+        protected abstract void OnDeselected();
+
+
+
 
     }
-
-    protected virtual void OnEnable()
-    {
-        leanSelectableByFinger.OnSelected.AddListener(OnSelectedAction);
-        leanSelectableByFinger.OnDeselected.AddListener(OnDeselectedAction);
-    }
-
-    protected virtual void OnDisable()
-    {
-        leanSelectableByFinger.OnSelected.RemoveListener(OnSelectedAction);
-        leanSelectableByFinger.OnDeselected.RemoveListener(OnDeselectedAction);
-    }
-
-    protected abstract void OnSelected();
-
-    protected abstract void OnDeselected();
-
-
-
 
 }
